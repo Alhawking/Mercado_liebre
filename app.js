a772687973c35130fa7848988d66b596151e8caa
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-01 23:57:06 -0500

    Cambio en el entry point para uso completo de recursos estáticos

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use('/public/img', express.static(__dirname + '/public/img'));
-app.use('/public/CSS', express.static(__dirname + '/public/CSS'));
+app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
 app.listen(8080);
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/home.html')));
