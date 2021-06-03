e2c487819f67cc8c314f0a2a50e4b50d52bf349d
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-03 13:00:43 -0500

    Modificacion del puerto para deploy y creación y configuración de Procfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
-app.listen(8080);
+app.listen(process.env.PORT || 3000);
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + '/views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + '/views/login.html')));
