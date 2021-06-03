c8822aa96718196ee99807057657cd90362f747c
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-03 13:40:04 -0500

    Modificaciones a app.js t Procfile

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Corriendo en el servidor ${server.address().port}`)
+});
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + '/views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + '/views/login.html')));
