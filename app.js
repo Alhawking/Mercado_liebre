064d2f7e2576105b0658417e70cc694f0aca4546
alhawking <alfredo.ledesma0605@gmail.com>
2021-06-03 14:09:17 -0500

    Modificación a mensaje app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Corriendo en el servidor ${server.address().port}`)
+    console.log(`Corriendo servidor`);
 });
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + '/views/register.html')));
